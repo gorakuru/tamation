7cf980ff10064e7058e919cea535192399b0fbbc
gorakuru <gorakuru@users.noreply.github.com>
2018-01-06 01:03:28 +0900

    Create irsend.ino.ino

---
+++ irsend.ino.ino
@@ -0,0 +1,125 @@
+uint16_t LivingTVPower[] = {9056, 4388,  650, 468,  656, 468,  650, 472,  650, 472,  652, 472,  650, 472,  650, 1586,  650, 472,  676, 1560,  650, 1586,  650, 1586,  750, 1488,  646, 1586,  652, 1586,  650, 472,  650, 1586,  650, 472,  676, 1560,  650, 468,  650, 472,  652, 1586,  676, 446,  650, 472,  650, 468,  680, 1562,  652, 466,  656, 1586,  646, 1586,  650, 472,  650, 1588,  650, 1586,  650, 1586,  650, 39674,  9078, 2130,  620};  // NEC 2FD48B7
+uint16_t LivingTVVolUp[] = {9028, 4410,  656, 468,  630, 492,  630, 494,  630, 494,  628, 494,  628, 488,  630, 1608,  630, 494,  628, 1608,  630, 1608,  650, 1588,  628, 1608,  626, 1608,  630, 1612,  650, 468,  630, 1610,  628, 492,  630, 1608,  628, 494,  624, 1608,  630, 1612,  624, 494,  630, 492,  630, 494,  652, 1586,  630, 494,  626, 1612,  626, 492,  630, 494,  706, 1532,  630, 1608,  630, 1608,  628, 39694,  9032, 2172,  630};  // NEC 2FD58A7
+uint16_t LivingTVVolDown[] = {9032, 4406,  654, 472,  650, 472,  624, 494,  654, 468,  630, 494,  628, 494,  628, 1608,  650, 472,  650, 1586,  650, 1584,  628, 1612,  626, 1608,  630, 1608,  652, 1586,  630, 492,  650, 1588,  650, 472,  626, 1612,  650, 1586,  626, 1608,  628, 1608,  628, 494,  650, 472,  650, 468,  630, 1608,  630, 494,  630, 494,  650, 472,  652, 470,  652, 1582,  630, 1608,  630, 1608,  630, 39696,  9032, 2202,  620};  // NEC 2FD7887
+uint16_t LivingLightOn[] = {9030, 4342,  660, 1552,  686, 420,  686, 446,  660, 446,  684, 1528,  658, 472,  684, 1526,  680, 450,  656, 452,  680, 1528,  684, 1552,  684, 420,  684, 1552,  656, 1558,  680, 1552,  686, 424,  680, 422,  684, 420,  684, 446,  684, 420,  684, 1526,  686, 446,  686, 420,  684, 422,  684, 446,  634, 472,  660, 446,  680, 424,  680, 450,  654, 446,  686, 420,  686, 446,  654, 1552,  684, 426,  654, 446,  684, 446,  658, 446,  660, 446,  660, 472,  634, 472,  656, 450,  654, 450,  680, 450,  630, 476,  656, 450,  654, 452,  654, 478,  628, 476,  654, 450,  654, 450,  656, 1578,  660, 1552,  656, 476,  656, 1558,  654, 1578,  638, 472,  630};  // UNKNOWN D2B4BE97
+uint16_t LivingLightOff[] = {9002, 4368,  634, 1578,  660, 446,  656, 476,  628, 476,  656, 1552,  660, 472,  660, 1552,  654, 476,  628, 476,  654, 1554,  658, 1578,  656, 450,  682, 1552,  658, 1552,  686, 1552,  684, 422,  658, 446,  684, 420,  686, 446,  686, 420,  684, 1528,  684, 446,  684, 422,  658, 446,  660, 472,  634, 472,  684, 420,  684, 420,  660, 472,  660, 446,  660, 446,  660, 472,  634, 472,  684, 1528,  654, 450,  680, 452,  680, 426,  680, 424,  680, 450,  656, 446,  686, 420,  684, 420,  686, 446,  634, 472,  660, 446,  660, 446,  680, 450,  654, 450,  682, 1526,  684, 1554,  684, 1528,  684, 1554,  660, 446,  654, 1584,  658, 1548,  684, 426,  680};  // UNKNOWN 1EA4B5CB
+uint16_t YoushituLightOn[] = {9050, 4440,  660, 472,  654, 1604,  654, 476,  630, 498,  634, 498,  654, 476,  654, 476,  654, 1600,  660, 1600,  634, 498,  634, 1626,  634, 1626,  654, 472,  660, 1600,  658, 1600,  654, 476,  654, 472,  634, 1626,  654, 1604,  656, 470,  660, 470,  656, 1604,  654, 476,  654, 1604,  634, 1620,  634, 498,  634, 496,  656, 1600,  660, 1600,  634, 496,  634, 1626,  634, 498,  630, 41322,  9126, 2112,  684};  // NEC 41B6659A
+uint16_t YoushituLightOff[] = {9048, 4440,  684, 446,  634, 1624,  634, 498,  634, 496,  654, 472,  658, 472,  634, 496,  656, 1602,  656, 1602,  656, 476,  630, 1630,  628, 1628,  656, 472,  660, 1600,  634, 1626,  634, 470,  680, 476,  634, 1624,  634, 1626,  654, 1604,  656, 1604,  654, 1604,  630, 496,  634, 1626,  634, 1624,  656, 476,  656, 476,  630, 446,  684, 498,  654, 476,  654, 1604,  654, 476,  630, 41340,  9048, 2222,  630};  // NEC 41B67D82
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+const char *ssid = "******";
+const char *password = "******";
+
+ESP8266WebServer server ( 80 );
+IRsend irsend(14);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  irsend.begin();
+  Serial.println("");
+
+  //wait for connection
+  while( WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleRoot() {
+  char temp[800];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  char message[20];
+  String(server.arg(0)).toCharArray(message,20);
+
+  Serial.println(server.arg(0));
+  if(server.arg(0).indexOf("LivingLightOn") != -1){
+    int len = sizeof(LivingLightOn) / sizeof(uint16_t);
+    irsend.sendRaw(LivingLightOn, len, 38);
+  }
+  else if(server.arg(0).indexOf("LivingLightOff") != -1){
+    int len = sizeof(LivingLightOff) / sizeof(uint16_t);
+    irsend.sendRaw(LivingLightOff, len, 38);
+  }
+  else if(server.arg(0).indexOf("YoushituLightOn") != -1){
+    int len = sizeof(YoushituLightOn) / sizeof(uint16_t);
+    irsend.sendRaw(YoushituLightOn, len, 38);
+  }
+  else if(server.arg(0).indexOf("YoushituLightOff") != -1){
+    int len = sizeof(YoushituLightOff) / sizeof(uint16_t);
+    irsend.sendRaw(YoushituLightOff, len, 38);
+  }
+  else if(server.arg(0).indexOf("LivingTVPower") != -1){
+    int len = sizeof(LivingTVPower) / sizeof(uint16_t);
+    irsend.sendRaw(LivingTVPower, len, 38);
+  }  
+  else if(server.arg(0).indexOf("LivingTVVolUp") != -1){
+    int len = sizeof(LivingTVVolUp) / sizeof(uint16_t);
+    irsend.sendRaw(LivingTVVolUp, len, 38);
+  }
+  else if(server.arg(0).indexOf("LivingTVVolDown") != -1){
+    int len = sizeof(LivingTVVolDown) / sizeof(uint16_t);
+    irsend.sendRaw(LivingTVVolDown, len, 38);
+  }
+  
+  snprintf ( temp, 800,
+
+"<html>\
+  <head>\
+    <title>ESPr IR Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>ESPr IR DEMO</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+  </body>\
+</html>",
+
+    hr, min % 60, sec % 60 ,message
+    
+  );
+  server.send ( 200, "text/html", temp );
+}
+
+void handleNotFound() {
+
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+  server.send ( 404, "text/plain", message );
+
+}

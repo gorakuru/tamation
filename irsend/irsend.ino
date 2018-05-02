7f8adfae9771c0abbede1c8ab61e3aeefe201cbd
gorakuru <gorakuru@users.noreply.github.com>
2018-05-02 15:17:46 +0900

    mqttベースに変更

--- irsend/irsend.ino
+++ irsend/irsend.ino
@@ -1,27 +1,64 @@
-uint16_t LivingTVPower[] = {9056, 4388,  650, 468,  656, 468,  650, 472,  650, 472,  652, 472,  650, 472,  650, 1586,  650, 472,  676, 1560,  650, 1586,  650, 1586,  750, 1488,  646, 1586,  652, 1586,  650, 472,  650, 1586,  650, 472,  676, 1560,  650, 468,  650, 472,  652, 1586,  676, 446,  650, 472,  650, 468,  680, 1562,  652, 466,  656, 1586,  646, 1586,  650, 472,  650, 1588,  650, 1586,  650, 1586,  650, 39674,  9078, 2130,  620};  // NEC 2FD48B7
-uint16_t LivingTVVolUp[] = {9028, 4410,  656, 468,  630, 492,  630, 494,  630, 494,  628, 494,  628, 488,  630, 1608,  630, 494,  628, 1608,  630, 1608,  650, 1588,  628, 1608,  626, 1608,  630, 1612,  650, 468,  630, 1610,  628, 492,  630, 1608,  628, 494,  624, 1608,  630, 1612,  624, 494,  630, 492,  630, 494,  652, 1586,  630, 494,  626, 1612,  626, 492,  630, 494,  706, 1532,  630, 1608,  630, 1608,  628, 39694,  9032, 2172,  630};  // NEC 2FD58A7
-uint16_t LivingTVVolDown[] = {9032, 4406,  654, 472,  650, 472,  624, 494,  654, 468,  630, 494,  628, 494,  628, 1608,  650, 472,  650, 1586,  650, 1584,  628, 1612,  626, 1608,  630, 1608,  652, 1586,  630, 492,  650, 1588,  650, 472,  626, 1612,  650, 1586,  626, 1608,  628, 1608,  628, 494,  650, 472,  650, 468,  630, 1608,  630, 494,  630, 494,  650, 472,  652, 470,  652, 1582,  630, 1608,  630, 1608,  630, 39696,  9032, 2202,  620};  // NEC 2FD7887
-uint16_t LivingLightOn[] = {9030, 4342,  660, 1552,  686, 420,  686, 446,  660, 446,  684, 1528,  658, 472,  684, 1526,  680, 450,  656, 452,  680, 1528,  684, 1552,  684, 420,  684, 1552,  656, 1558,  680, 1552,  686, 424,  680, 422,  684, 420,  684, 446,  684, 420,  684, 1526,  686, 446,  686, 420,  684, 422,  684, 446,  634, 472,  660, 446,  680, 424,  680, 450,  654, 446,  686, 420,  686, 446,  654, 1552,  684, 426,  654, 446,  684, 446,  658, 446,  660, 446,  660, 472,  634, 472,  656, 450,  654, 450,  680, 450,  630, 476,  656, 450,  654, 452,  654, 478,  628, 476,  654, 450,  654, 450,  656, 1578,  660, 1552,  656, 476,  656, 1558,  654, 1578,  638, 472,  630};  // UNKNOWN D2B4BE97
-uint16_t LivingLightOff[] = {9002, 4368,  634, 1578,  660, 446,  656, 476,  628, 476,  656, 1552,  660, 472,  660, 1552,  654, 476,  628, 476,  654, 1554,  658, 1578,  656, 450,  682, 1552,  658, 1552,  686, 1552,  684, 422,  658, 446,  684, 420,  686, 446,  686, 420,  684, 1528,  684, 446,  684, 422,  658, 446,  660, 472,  634, 472,  684, 420,  684, 420,  660, 472,  660, 446,  660, 446,  660, 472,  634, 472,  684, 1528,  654, 450,  680, 452,  680, 426,  680, 424,  680, 450,  656, 446,  686, 420,  684, 420,  686, 446,  634, 472,  660, 446,  660, 446,  680, 450,  654, 450,  682, 1526,  684, 1554,  684, 1528,  684, 1554,  660, 446,  654, 1584,  658, 1548,  684, 426,  680};  // UNKNOWN 1EA4B5CB
-uint16_t YoushituLightOn[] = {9050, 4440,  660, 472,  654, 1604,  654, 476,  630, 498,  634, 498,  654, 476,  654, 476,  654, 1600,  660, 1600,  634, 498,  634, 1626,  634, 1626,  654, 472,  660, 1600,  658, 1600,  654, 476,  654, 472,  634, 1626,  654, 1604,  656, 470,  660, 470,  656, 1604,  654, 476,  654, 1604,  634, 1620,  634, 498,  634, 496,  656, 1600,  660, 1600,  634, 496,  634, 1626,  634, 498,  630, 41322,  9126, 2112,  684};  // NEC 41B6659A
-uint16_t YoushituLightOff[] = {9048, 4440,  684, 446,  634, 1624,  634, 498,  634, 496,  654, 472,  658, 472,  634, 496,  656, 1602,  656, 1602,  656, 476,  630, 1630,  628, 1628,  656, 472,  660, 1600,  634, 1626,  634, 470,  680, 476,  634, 1624,  634, 1626,  654, 1604,  656, 1604,  654, 1604,  630, 496,  634, 1626,  634, 1624,  656, 476,  656, 476,  630, 446,  684, 498,  654, 476,  654, 1604,  654, 476,  630, 41340,  9048, 2222,  630};  // NEC 41B67D82
-uint16_t BedroomLightButton[] = {10334, 5024,  726, 1852,  726, 1852,  726, 1852,  726, 578,  726, 566,  726, 1852,  730, 1852,  726, 1862,  726, 564,  726, 568,  752, 1826,  726, 1860,  726, 570,  752, 538,  752, 544,  752, 548,  752, 1826,  854, 1724,  726, 1852,  730, 574,  726, 566,  730, 548,  768, 540,  752, 1838,  726, 564,  752, 540,  756, 538,  752, 1836,  752, 1826,  726, 1852,  730, 1852,  726, 564,  722, 45844,  10334, 2458,  722};  // NEC E730E11E                                
-uint16_t BedroomTVPower[] = {9022, 4404,  634, 502,  632, 502,  630, 502,  632, 502,  630, 1612,  632, 502,  630, 1608,  634, 502,  654, 1586,  634, 1608,  634, 1608,  634, 1608,  634, 502,  630, 1612,  630, 502,  656, 1586,  634, 1608,  634, 1608,  634, 1608,  634, 502,  634, 1608,  632, 502,  628, 502,  634, 502,  634, 502,  630, 502,  634, 502,  630, 1612,  630, 502,  634, 1608,  634, 1608,  634, 1608,  634, 39360,  9020, 2164,  634};  // NEC AF5E817
-uint16_t BedroomTVVolUp[] = {9070, 4358,  680, 450,  656, 480,  656, 480,  654, 476,  654, 1586,  680, 454,  656, 1582,  660, 476,  656, 1586,  680, 1562,  654, 1588,  680, 1562,  654, 480,  680, 1556,  684, 450,  656, 1586,  680, 454,  680, 1562,  680, 450,  656, 480,  680, 1560,  680, 454,  656, 476,  656, 480,  680, 1556,  660, 476,  654, 1586,  656, 1586,  654, 480,  654, 1582,  658, 1584,  658, 1582,  660, 39334,  9046, 2138,  680};  // NEC AF548B7
-uint16_t BedroomTVVolDown[] = {9042, 4384,  654, 480,  628, 502,  654, 480,  654, 476,  654, 1586,  634, 502,  654, 1588,  654, 480,  654, 1582,  660, 1582,  660, 1582,  634, 1608,  660, 476,  654, 1586,  656, 480,  654, 1584,  658, 1582,  658, 476,  654, 1586,  656, 480,  654, 1586,  656, 476,  656, 480,  656, 476,  656, 480,  654, 1586,  656, 480,  650, 1586,  654, 480,  656, 1588,  654, 1586,  656, 1586,  654, 39338,  9044, 2138,  654};  // NEC AF5A857
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
-const char *ssid = "****";
-const char *password = "****";
+/* PubSubClient.h の中の MQTT_MAX_PACKET_SIZE を大きくする */
 
-ESP8266WebServer server ( 80 );
+const char *ssid = "*********";
+const char *password = "*********";
+const char *token = "token:*********";
+const char *mqttserver = "mqtt.beebotte.com";
+
+
+
 IRsend irsend(14);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+
+// メッセージを受信したときのコールバック関数
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("callback invoked");
+
+  StaticJsonBuffer<2048> jsonBuffer;
+  uint16_t codebuffer[100];
+
+  payload[length] = '\0';
+  String msg = String((char*) payload);
+  Serial.println(msg);
+
+  // jsonデコード
+  JsonObject& object = jsonBuffer.parseObject(msg);
+  if (!object.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+
+  // コマンド取得
+  const char* command = object["data"][0]["cmd"];
+  String cmd = String(command);
+  Serial.println(cmd);
+
+  // コード取得
+  int codelength =  object["data"][0]["code"].size();
+  Serial.println(codelength);
+  if (codelength < 100) {
+    for (int i = 0; i < codelength; i++) {
+      codebuffer[i] = object["data"][0]["code"][i];
+    }
+  } else {
+    Serial.println("code is tooooo long!");
+    return;
+  }
+
+  // シグナル送信！
+  Serial.println("send signal.");
+  irsend.sendRaw(codebuffer, codelength, 38);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -30,7 +67,7 @@ void setup() {
   Serial.println("");
 
   //wait for connection
-  while( WiFi.status() != WL_CONNECTED){
+  while ( WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -40,111 +77,27 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/",handleRoot);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
+  client.setServer(mqttserver, 1883);
+  client.setCallback(callback);
 }
+
 
 void loop() {
-  server.handleClient();
+
+  if (!!!client.connected()) {
+    Serial.print("Reconnecting client to ");
+    Serial.println(mqttserver);
+    while (!!!client.connect("test", token, "")) {
+      Serial.print(".");
+      delay(500);
+    }
+    if (client.subscribe("IFTTT/ir")) {
+      Serial.println("Subscribe ok");
+    } else {
+      Serial.println("Subscribe failed");
+    }
+    Serial.println();
+  }
+  client.loop();
 }
 
-void handleRoot() {
-  char temp[800];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  char message[20];
-  String(server.arg(0)).toCharArray(message,20);
-
-  Serial.println(server.arg(0));
-  if(server.arg(0).indexOf("LivingLightOn") != -1){
-    int len = sizeof(LivingLightOn) / sizeof(uint16_t);
-    irsend.sendRaw(LivingLightOn, len, 38);
-  }
-  else if(server.arg(0).indexOf("LivingLightOff") != -1){
-    int len = sizeof(LivingLightOff) / sizeof(uint16_t);
-    irsend.sendRaw(LivingLightOff, len, 38);
-  }
-  else if(server.arg(0).indexOf("YoushituLightOn") != -1){
-    int len = sizeof(YoushituLightOn) / sizeof(uint16_t);
-    irsend.sendRaw(YoushituLightOn, len, 38);
-  }
-  else if(server.arg(0).indexOf("YoushituLightOff") != -1){
-    int len = sizeof(YoushituLightOff) / sizeof(uint16_t);
-    irsend.sendRaw(YoushituLightOff, len, 38);
-  }
-  else if(server.arg(0).indexOf("LivingTVPower") != -1){
-    int len = sizeof(LivingTVPower) / sizeof(uint16_t);
-    irsend.sendRaw(LivingTVPower, len, 38);
-  }  
-  else if(server.arg(0).indexOf("LivingTVVolUp") != -1){
-    int len = sizeof(LivingTVVolUp) / sizeof(uint16_t);
-    irsend.sendRaw(LivingTVVolUp, len, 38);
-  }
-  else if(server.arg(0).indexOf("LivingTVVolDown") != -1){
-    int len = sizeof(LivingTVVolDown) / sizeof(uint16_t);
-    irsend.sendRaw(LivingTVVolDown, len, 38);
-  }
-  else if(server.arg(0).indexOf("bedroomTVVolUp") != -1){
-    int len = sizeof(BedroomTVVolUp) / sizeof(uint16_t);
-    irsend.sendRaw(BedroomTVVolUp, len, 38);
-  }
-  else if(server.arg(0).indexOf("BedroomTVVolDown") != -1){
-    int len = sizeof(BedroomTVVolDown) / sizeof(uint16_t);
-    irsend.sendRaw(BedroomTVVolDown, len, 38);
-  }
-  else if(server.arg(0).indexOf("BedroomTVPower") != -1){
-    int len = sizeof(BedroomTVPower) / sizeof(uint16_t);
-    irsend.sendRaw(BedroomTVPower, len, 38);
-  }
-  else if(server.arg(0).indexOf("BedroomLightButton") != -1){
-    int len = sizeof(BedroomLightButton) / sizeof(uint16_t);
-    irsend.sendRaw(BedroomLightButton, len, 38);
-  }
-
-
-
-
-
-  
-  snprintf ( temp, 800,
-
-"<html>\
-  <head>\
-    <title>ESPr IR Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>ESPr IR DEMO</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-  </body>\
-</html>",
-
-    hr, min % 60, sec % 60 ,message
-    
-  );
-  server.send ( 200, "text/html", temp );
-}
-
-void handleNotFound() {
-
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-  }
-  server.send ( 404, "text/plain", message );
-
-}
